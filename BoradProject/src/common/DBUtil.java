bb839ab28acb36922cabb03c733617dbc99125eb
[DongJinJeong] <dlswho19@gmail.com>
2020-09-22 11:27:09 +0900

    동진DB 접근 DBUtill

--- BoradProject/src/common/DBUtil.java
+++ BoradProject/src/common/DBUtil.java
@@ -9,7 +9,7 @@ import java.sql.SQLException;
 public class DBUtil {
 
 	static final String DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
-	static final String url = "jdbc:mysql://127.0.0.1:3306/dongjindb?serverTimezone=UTC";
+	static final String url = "jdbc:mysql://192.168.70.73:3306/dongjindb?serverTimezone=UTC";
 	static final String user = "dongjin";
 	static final String password = "dongjinpw";
 	// sdkmsdlksmdlksmdlksmdaskd akjd ajdajsd
