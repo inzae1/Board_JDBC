76166250b91f9bdf3c1e36a096b066b46b729e9f
ssdred1250 <ssdred1250@naver.com>
2020-09-22 12:11:24 +0900

    please merge

--- BoradProject/src/common/DBUtil.java
+++ BoradProject/src/common/DBUtil.java
@@ -12,19 +12,20 @@ public class DBUtil {
 	static final String url = "jdbc:mysql://192.168.70.73:3306/dongjindb?serverTimezone=UTC";
 	static final String user = "dongjin";
 	static final String password = "dongjinpw";
-	// sdkmsdlksmdlksmdlksmdaskd akjd ajdajsd
-	public static Connection getConnection() throws Exception{
-		return getConnection(url, user, password);		
+
+	public static Connection getConnection() throws Exception {
+		return getConnection(url, user, password);
 	}
-	public static Connection getConnection(String url,String user, String password) throws Exception{
+
+	public static Connection getConnection(String url, String user, String password) throws Exception {
 		Class.forName(DRIVER_NAME);
 		Connection conn = DriverManager.getConnection(url, user, password);
 		return conn;
 	}
-	
-	//close 
+
+	// close
 	public static void close(Connection conn) {
-		if(conn != null) {
+		if (conn != null) {
 			try {
 				conn.close();
 			} catch (SQLException e) {
@@ -32,8 +33,9 @@ public class DBUtil {
 			}
 		}
 	}
+
 	public static void close(Connection conn, PreparedStatement ps) {
-		if(ps != null) {
+		if (ps != null) {
 			try {
 				ps.close();
 			} catch (SQLException e) {
@@ -42,8 +44,9 @@ public class DBUtil {
 		}
 		close(conn);
 	}
+
 	public static void close(Connection conn, PreparedStatement ps, ResultSet rs) {
-		if(rs != null) {
+		if (rs != null) {
 			try {
 				rs.close();
 			} catch (SQLException e) {
@@ -52,8 +55,9 @@ public class DBUtil {
 		}
 		close(conn, ps);
 	}
+
 	public static void main(String[] args) {
-		
+
 		Connection conn = null;
 		try {
 			conn = DBUtil.getConnection();
@@ -61,6 +65,6 @@ public class DBUtil {
 			e.printStackTrace();
 		}
 		System.out.println(conn);
-		
+
 	}
 }
