7ab74bd94a6b7069b2f5d323f8da38eb85d6a2c0
DongJinJeong <ehd5719@gmail.com>
2020-09-23 11:03:16 +0900

    board

--- BoradProject/src/control/LoginServlet.java
+++ BoradProject/src/control/LoginServlet.java
@@ -23,25 +23,27 @@ public class LoginServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		
 		HttpSession session = request.getSession();
 		//로그인
 		String id = request.getParameter("id");
 		String pw = request.getParameter("pw");
 
 		UserDAO dao = new UserDAO();
+		response.setContentType("text/html; charset=utf-8");
 		PrintWriter out = response.getWriter();
 		int user = dao.getUser(id, pw);
 		
 		if (user == -1) {
 			// 아이디가 없을 때 회원 가입으로 가기 response.sendRedirect()
-			out.println("<script>");
-			out.println("alert('가입되지 않은 아이디입니다.)");
+			out.println("<script>alert('가입되지 않은 아이디입니다.'); history.back();");
 			out.println("</script>");
+			out.flush();
 		} else if (user == 0) {
 			// 비밀번호 불일치 할 때 비밀번호 찾기로 가기 response.sendRedirect() 
-			out.println("<script>");
-			out.println("alert('비밀번호가 틀렸습니다.)");
+			out.println("<script>alert('비밀번호 잘못 입력하셨습니다.'); history.back();");
 			out.println("</script>");
+			out.flush();
 		} else if (user == 1) {
 			// 로그인 성공 시 자유게시판 페이지로 이동 response.sendRedirect("main.jsp")
 			session.setAttribute(id, pw);
