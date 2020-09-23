5129f9cf9289a4206b7a7ebe13c903078b4d40db
DongJinJeong <ehd5719@gmail.com>
2020-09-23 16:57:35 +0900

    main

--- BoradProject/src/tvxq/borad/controller/FindId.java
+++ BoradProject/src/tvxq/borad/controller/FindId.java
@@ -11,9 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import tvxq.borad.dao.UserDAO;
 
-/**
- * Servlet implementation class FindId
- */
+
 @WebServlet("/findId")
 public class FindId extends HttpServlet {
 	private static final long serialVersionUID = 1L;
